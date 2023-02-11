f722b09f25a6c69240a571256b36531f47a156ca
yanhao.xu <yanhao.xu@outlook.com>
2023-02-11 11:41:35 +0800

    feature(rabbitmq-demo): demo source update

--- rabbitmq-demo/src/main/java/com/github/xuyh/ConsumeDirectMQ.java
+++ rabbitmq-demo/src/main/java/com/github/xuyh/ConsumeDirectMQ.java
@@ -17,7 +17,6 @@ public class ConsumeDirectMQ {
     factory.setUsername("admin");
     factory.setPassword("admin");
     factory.setVirtualHost("/test");
-
     // 不使用try-with-resource, 因为消费者是一直等待新的消息到来, 然后按照
     // 我们设定的逻辑进行处理, 所以这里不能在定义完成之后就关闭连接
     Connection connection = factory.newConnection();
@@ -25,6 +24,14 @@ public class ConsumeDirectMQ {
     // 创建一个基本的消费者
     channel.basicConsume("yyds", false, (s, delivery) -> {
       System.out.println(new String(delivery.getBody()));
+      // basicAck是确认应答,第一个参数是当前消息的标签,后面的参数是
+      // 是否批量处理消息队列中所有的消息,如果为false表示的只处理当前消息
+      channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+      //basicNack是拒绝应达,最后一个参数表示是否是将当前消息放回队列
+      // 如果为false,那么消息就会被丢弃
+      //      channel.basicNack(delivery.getEnvelope().getDeliveryTag(), false, false);
+      // 和上面一样,最后一个参数为false,这里省略了
+      //      channel.basicReject(delivery.getEnvelope().getDeliveryTag(), false);
     }, s -> {
     });
 
